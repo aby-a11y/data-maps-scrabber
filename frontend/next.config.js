68f130dc90f5079a9c161725e4ba6fef2222259e
Abhishek kumar <aby@pixelglobalit.com>
2026-03-31 14:53:16 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -34,7 +34,8 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: "http://127.0.0.1:8000/api/:path*", // Proxy to Backend
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 
+                        "http://127.0.0.1:8000"}/api/:path*`,          // Proxy to Backend
       },
     ]
   },
