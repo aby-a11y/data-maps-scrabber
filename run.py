a9d19d6518146510d31d777fb33d83f322b600fe
Abhishek kumar <aby@pixelglobalit.com>
2026-04-01 11:29:46 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import backend.scrapers
-from botasaurus_server.run import run
+from botasaurus_server.server import Server
 
 if __name__ == "__main__":
-    run()
+    Server.run(port=8000, host="0.0.0.0")
