be7883c1eed7ccc50e8ed0034e29da3140e37957
Abhishek kumar <aby@pixelglobalit.com>
2026-04-01 12:02:38 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+import os
 import backend.scrapers
-from botasaurus_server.run import run
+from botasaurus_server.app import app
+from bottle import run as bottle_run
 
 if __name__ == "__main__":
-    run()
+    port = int(os.environ.get("PORT", 8000))
+    bottle_run(app, host="0.0.0.0", port=port)
